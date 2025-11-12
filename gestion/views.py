38f10373269b3f660caaef5384a65491e5c20d96
xoxo-yf <onlyxoxo36@gmail.com>
2025-11-12 10:42:02 +0300

    NAFTA

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'gestion/main.html
+    return render(request, 'gestion/main.html')
+                  
 # C’est ici qu’on met la logique — afficher la liste, ajouter, modifier, supprimer..
